963cef2615eb2fb8e53910a4ac4acf0f8cd9a6af
dat-iro <138441382+dat-iro@users.noreply.github.com>
2023-09-15 11:54:03 +0200

    solution

--- exercise.js
+++ exercise.js
@@ -8,5 +8,11 @@ const person2 = person1;
 
 // Modify the property `firstName` of the `person2` in "Simon".
 
+person2.firstName = "Simon";
+
 console.log(person1);
 console.log(person2);
+
+//person1 e person2 puntano entrambe allo stesso oggetto, pertanto modificando il valore
+//di una delle due variabili si cambiano i valori presenti nell'oggetto puntato da entrambe,
+//quindi in questo caso firstName diventerà per entrambi "Simon".
